a730b408a9275c49ff455253c54f6fac9eec549e
Arthur Bispo <arthurbispo94@gmail.com>
2022-03-07 15:34:13 -0300

    ensure that validator recive the same email that is sent

--- src/presentation/utils/email-validator.spec.js
+++ src/presentation/utils/email-validator.spec.js
@@ -25,4 +25,9 @@ describe('Email Validator', () => {
     const isEmailValid = sut.isValid('invalid_email@email.com')
     expect(isEmailValid).toBe(false)
   })
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+    sut.isValid('any_email@email.com')
+    expect(sut.validator.email).toBe('any_email@email.com')
+  })
 })
